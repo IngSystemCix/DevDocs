462696ecfb07add655bf8498c42155ef216dcccd
IngSystemCix <juanromerocollazos@gmail.com>
2024-09-25 23:49:15 -0500

    update

--- src/react/CodeComponent.tsx
+++ src/react/CodeComponent.tsx
@@ -1,18 +1,22 @@
+// src/components/CodeComponent.tsx
 import React from "react";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedDarkAtom } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { dracula, solarizedlight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface CodeComponentProps {
   codeString: string;
   language: string;
+  style?: 'dracula' | 'solarizedlight';
 }
 
-const CodeComponent: React.FC<CodeComponentProps> = ({ codeString, language }) => {
+const CodeComponent: React.FC<CodeComponentProps> = ({ codeString, language, style = 'dracula' }) => {
+  const selectedStyle = style === 'solarizedlight' ? solarizedlight : dracula;
+
   return (
-    <SyntaxHighlighter language={language} style={solarizedDarkAtom}>
-      {codeString}
+    <SyntaxHighlighter language={language} style={selectedStyle}>
+      {codeString.trim()}
     </SyntaxHighlighter>
   );
-}
+};
 
 export default CodeComponent;
