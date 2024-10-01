81477642249f9e2c80a25d69bafa94203b7e749d
IngSystemCix <juanromerocollazos@gmail.com>
2024-10-01 01:24:24 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,4 +16,5 @@ export default defineConfig({
     format: 'directory', // Usar directorios en lugar de archivos .html
   },
   output: 'server',
+  adapter: node({ mode: 'standalone' }), // Example option, adjust as needed
 });
