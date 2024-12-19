c430b6a723305474914ed6df7ebef631ce20a4b2
IngSystemCix <juanromerocollazos@gmail.com>
2024-12-18 22:28:56 -0500

    feat: fix bugs in copy code

--- src/react/CodeComponent.tsx
+++ src/react/CodeComponent.tsx
@@ -1,66 +1,71 @@
 import React, { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dracula, solarizedlight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import {
+  dracula,
+  solarizedlight,
+} from "react-syntax-highlighter/dist/cjs/styles/prism";
+import pkg from "react-copy-to-clipboard"; // Importa el paquete de forma predeterminada
+const { CopyToClipboard } = pkg; // Desestructura CopyToClipboard del paquete
 
 interface CodeComponentProps {
-    codeString: string;
-    language: string;
-    style?: "dracula" | "solarizedlight";
+  codeString: string;
+  language: string;
+  style?: "dracula" | "solarizedlight";
 }
 
-const CodeComponent: React.FC<CodeComponentProps> = ({ codeString, language, style = "dracula" }) => {
-    const [copied, setCopied] = useState(false);
-    const selectedStyle = style === "solarizedlight" ? solarizedlight : dracula;
+const CodeComponent: React.FC<CodeComponentProps> = ({
+  codeString,
+  language,
+  style = "dracula",
+}) => {
+  const [copied, setCopied] = useState(false);
+  const selectedStyle = style === "solarizedlight" ? solarizedlight : dracula;
 
-    const handleCopy = async () => {
-        try {
-            await (navigator.clipboard as Clipboard).writeText(codeString);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000);
-        } catch (err) {
-            console.error("Error copying text: ", err);
-            if (navigator.clipboard) {
-              // Si navigator.clipboard está presente pero falla, es probable que sea un problema de permisos o HTTPS
-              alert("Failed to copy text. Please ensure you are using HTTPS and have granted clipboard permissions.");
-            } else {
-              // Si navigator.clipboard no está presente, usar una solución alternativa (ej. execCommand, que está en desuso)
-              const textArea = document.createElement('textarea');
-              textArea.value = codeString;
-              document.body.appendChild(textArea);
-              textArea.focus();
-              textArea.select();
-              try {
-                await (navigator.clipboard as Clipboard).writeText(codeString);
-                setCopied(true);
-                setTimeout(() => setCopied(false), 2000);
-              } catch (copyError) {
-                console.error("Fallback copy failed:", copyError);
-                alert("Copying is not supported in this browser. Please copy manually.");
-              }
-              document.body.removeChild(textArea);
-            }
-        }
-    };
+  const handleCopy = () => {
+    console.log("Copying to clipboard...");
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
 
-    return (
-        <div className="relative">
-            <SyntaxHighlighter language={language} style={selectedStyle}>
-                {codeString.trim()}
-            </SyntaxHighlighter>
-            <button
-                onClick={handleCopy}
-                className="absolute top-2 right-2 px-3 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-600"
-                aria-label="Copy code"
-            >
-                <i className="bi bi-clipboard"></i>
-            </button>
-            {copied && (
-                <div className="absolute bottom-2 z-10 right-16 text-sm text-green-500">
-                    Copied!
-                </div>
-            )}
+  return (
+    <>
+      <div className="relative">
+        <SyntaxHighlighter language={language} style={selectedStyle}>
+          {codeString.trim()}
+        </SyntaxHighlighter>
+
+        <CopyToClipboard text={codeString} onCopy={handleCopy}>
+          <button
+            className="absolute z-50 top-2 right-2 px-3 py-2 bg-gray-800 text-white rounded-md hover:bg-gray-600"
+            aria-label="Copy code"
+          >
+            <i className="bi bi-clipboard"></i>
+          </button>
+        </CopyToClipboard>
+      </div>
+      {copied && (
+        <div
+          className="fixed bottom-4 right-4 z-30 flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Información</span>
+          <div>
+            <span className="font-medium">Código copiado con éxito!</span>{" "}
+            Puedes pegarlo donde lo necesites.
+          </div>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default CodeComponent;
